efa35c8c386d7162cf62b1a3958da14cb765516b
resta <resta.freelife@gmail.com>
2022-05-12 06:50:42 +0900

    ブログ詳細、トップ制作実績調整

--- src/js/swiper__top-works.js
+++ src/js/swiper__top-works.js
@@ -1,9 +1,9 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
   const swiper__topWorks = new Swiper('.js-topWorks-slider', {
     loop: true,
-    // autoplay: {
-    //   delay: 3000,
-    // },
+    autoplay: {
+      delay: 4000,
+    },
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
